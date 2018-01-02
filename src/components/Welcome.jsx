fce31f5c13b2607352169d500195affdf318a87c
Betty-Kebenei <kebeneibetty13@gmail.com>
2018-01-02 11:32:36 +0300

    [Feature] Add an index component.

---
+++ src/components/Welcome.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+// import { Button } from 'react-native';
+
+class Welcome extends React.Component {
+    render(){
+        return(
+            <div>
+                <h1>Welcome to the Shopping List Tracker</h1>  
+                <h2>Let's help you keep tracker on what you desire to spend your money on.</h2>
+                {/* <ButtonComponent>Button</ButtonComponent> */}
+            </div>
+        )
+    }
+}
+
+export default Welcome
