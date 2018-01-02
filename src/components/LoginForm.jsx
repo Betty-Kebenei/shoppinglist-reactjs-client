eff50eab03c97d0e83c2cab7fe9e825bd682546b
Betty-Kebenei <kebeneibetty13@gmail.com>
2018-01-02 12:46:12 +0300

    [Feature] Add controlled components to handle multiple inputs.

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,18 +1,32 @@
 import React from 'react';
 
 class LoginForm extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            email: '',
+            passowrd: ''
+        }
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+    handleChange(event){
+        this.setState({ [event.target.name]: event.target.value });
+    }
+    handleSubmit(event){
+        alert('You are successfully logged in!');
+        event.preventDefault();
+    }
     render(){
         return(
             <div>
                 <form onSubmit={this.handleSubmit}>
-                    <label>
-                        Username/Email:
-                        <input type="text" name="email" />
-                    </label>
-                    <label>
-                        Password:
-                        <input type="text" name="password" />
-                    </label>
+                    <label> Username/Email: </label>
+                    <input type="text" name="email" onChange={this.handleChange} />
+                   
+                    <label> Password: </label>
+                    <input type="password" name="password" onChange={this.handleChange} />
+                    
                     <input type="submit" value="Submit" />
                     </form>
             </div>
