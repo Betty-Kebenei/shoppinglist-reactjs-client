3d268d34e7eb7cceb68a2972dc3981b16dcdada1
Betty-Kebenei <kebeneibetty13@gmail.com>
2018-01-02 12:51:31 +0300

    [Feature] Add multile input handle on change.

--- src/components/RegistrationForm.jsx
+++ src/components/RegistrationForm.jsx
@@ -3,12 +3,17 @@ import React from 'react';
 class RegistrationForm extends React.Component {
     constructor(props){
         super(props);
-        this.state = {value: ''};
+        this.state = {
+            username: '',
+            email: '',
+            password: '',
+            confirm_password: ''
+        };
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
     handleChange(event){
-        this.setState({value: event.target.value});
+        this.setState({[event.target.name]: event.target.value});
     }
     handleSubmit(event){
         alert('You are successfully registered to Shopping List Tracker!');
@@ -19,16 +24,16 @@ class RegistrationForm extends React.Component {
             <div>
                 <form onSubmit={this.handleSubmit}>
                     <label>Username:</label>
-                    <input type="text" name="username" />
+                    <input type="text" name="username" onChange={this.handleChange} />
                     
                     <label>Email:</label>
-                    <input type="text" name="email" />
+                    <input type="text" name="email" onChange={this.handleChange} />
                     
                     <label>Password:</label>
-                    <input type="text" name="password" />
+                    <input type="password" name="password" onChange={this.handleChange} />
 
                     <label>Confirm Password:</label>
-                    <input type="text" name="confirm_password" />
+                    <input type="password" name="confirm_password" onChange={this.handleChange} />
                     
                     <input type="submit" value="Submit" />
                 </form>
