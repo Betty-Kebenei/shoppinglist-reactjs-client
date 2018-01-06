3d00486cfb59f80342e5ce0a3032bce16c5353cd
Betty-Kebenei <kebeneibetty13@gmail.com>
2018-01-06 12:57:02 +0300

    [FIXES] Update the registration form.
    Use a bootstrap modal to create a form.
    Clear data from the registration form after submitting.
    Add fields to hold errors when validating.
    Structure the handlechange method.

--- src/components/RegistrationForm.jsx
+++ src/components/RegistrationForm.jsx
@@ -7,45 +7,62 @@ class RegistrationForm extends React.Component {
             username: '',
             email: '',
             password: '',
-            confirm_password: ''
+            confirm_password: '',
+            signupformErrors: {username: '', email: '', password:''},
+            usernameValid: false,
+            emailValid: false,
+            passwordValid: false,
+            formValid: false
         };
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
     handleChange(event){
-        this.setState({[event.target.name]: event.target.value.toLowerCase()});
+        this.setState(
+            {[event.target.name]: event.target.value.toLowerCase()}
+        );
     }
     handleSubmit(event){
         alert('You are successfully registered to Shopping List Tracker!');
-        event.preventDefault();
+        this.setState({
+            username: '',
+            email: '',
+            password: '',
+            confirm_password: ''
+        });
+        
     }
+      
     render(){
         return(
             <div>
-                <form onSubmit={this.handleSubmit}>
-                    <div className="form-group">
-                    <label>Username:</label>
-                    <input type="text" name="username" onChange={this.handleChange} />
+                <div id="signupModal" className="modal fade" role="dialog">
+                    <div className="modal-dialog">
+                        <div className="modal-content">
+                            <div className="modal-header">
+                                <button type="button" className="close" data-dismiss="modal">&times;</button>
+                                <h2 className="modal-title">Sign Up</h2>
+                            </div>
+                            <div className="modal-body">
+                                <form className="signup" onSubmit={this.handleSubmit} >
+                                    Username:<br />
+                                    <input type="text" name="username" onChange={this.handleChange} /><br />
+                                    Email:<br />
+                                    <input type="text" name="email" onChange={this.handleChange} /><br />
+                                    Password:<br />
+                                    <input type="password" name="password" onChange={this.handleChange} /><br />
+                                    Confirm Password:<br />
+                                    <input type="password" name="confirm_password" onChange={this.handleChange} />
+                                    <input type="submit" value="Submit" />
+                                </form> 
+                            </div>
+                            <div className="modal-footer">
+                                <button type="button" className="btn btn-default" data-dismiss="modal">Close</button>
+                            </div>
+                        </div>
                     </div>
-                    
-                    <div className="form-group">
-                    <label>Email:</label>
-                    <input type="text" name="email" onChange={this.handleChange} />
-                    </div>
-
-                    <div className="form-group">
-                    <label>Password:</label>
-                    <input type="password" name="password" onChange={this.handleChange} />
-                    </div>
-
-                    <div className="form-group">
-                    <label>Confirm Password:</label>
-                    <input type="password" name="confirm_password" onChange={this.handleChange} />
-                    </div>
-                    
-                    <input type="submit" value="Submit" />
-                </form>
-            </div>
+                </div>
+            </div> 
         );
     }
 }
