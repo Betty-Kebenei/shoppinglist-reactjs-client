8ed734bf185e0b49ac2227ba54fc8efad8481235
Betty-Kebenei <kebeneibetty13@gmail.com>
2018-01-06 12:45:31 +0300

    [FEATURE] Add login component to the main component.
    Also removed unused imported components.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,15 +7,14 @@ import 'bootstrap/dist/css/bootstrap-theme.css';
 import React from 'react';
 import Welcome from './Welcome.jsx';
 import RegistrationForm from './RegistrationForm';
-// import Navbar from './Navbar.jsx';
-// import Footer from './Footer.jsx';
-
+import LoginForm from './LoginForm';
 export default class App extends React.Component {
   render() {
     return (
         <div style={{textAlign: 'center'}}>
             <Welcome />
-            <RegistrationForm />
+            <RegistrationForm /> 
+            <LoginForm />
         </div>);
   }
 }
