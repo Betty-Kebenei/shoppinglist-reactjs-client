58f4332188464f5e21a40fe34ee3e6bee52c7c8a
Betty-Kebenei <kebeneibetty13@gmail.com>
2018-01-09 21:01:49 +0300

    CHORE #154182716] Add router to define the path of each component.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,34 @@
-/*
-    ./client/components/App.jsx
-*/
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
+import '../static/index.css';
 
 import React from 'react';
-import Welcome from './Welcome.jsx';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from '../reducers';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import Header from './Header';
 import RegistrationForm from './RegistrationForm';
-import LoginForm from './LoginForm';
-export default class App extends React.Component {
-  render() {
-    return (
-        <div style={{textAlign: 'center'}}>
-            <Welcome />
-            <RegistrationForm /> 
-            <LoginForm />
-        </div>);
-  }
-}
+import ShoppingList from './shoppinglist';
+import ShoppinglistForm from './ShoppinglistForm';
+
+const App = () => (
+    <div>
+        <Provider store={createStore(reducers)}>
+           <BrowserRouter>
+                <div>
+                    <Switch>
+                        {/* <Header /> */}
+                        <Route path='/auth' component={RegistrationForm} />
+                        <Route path='/shoppinglists' component={ShoppinglistForm} />
+                        <Route path='/' component={ShoppingList} />
+                    </Switch>
+                </div> 
+            </BrowserRouter>
+        </Provider>
+    </div>
+
+)
+
+export default App;
