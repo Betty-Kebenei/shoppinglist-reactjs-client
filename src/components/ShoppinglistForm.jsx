09dabd99c451eb2ddc4ae436f870d759d3dd0ffa
Betty-Kebenei <kebeneibetty13@gmail.com>
2018-01-11 09:35:18 +0300

    [FEATURE #154016487] Update form using redux-form.

--- src/components/ShoppinglistForm.jsx
+++ src/components/ShoppinglistForm.jsx
@@ -1,33 +1,63 @@
-import React from 'react';
+import '../static/index.css';
 
+import React from 'react';
+import { Field, reduxForm } from 'redux-form';
+import { Link } from 'react-router-dom';
 class ShoppinglistForm extends React.Component{
-    constructor(props){
-        super(props);
-        this.state = {value: ''}
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
-    handleChange(event){
-        this.setState({value: event.target.value.toLowerCase()});
-    }
 
     handleSubmit(event){
         alert('Shopping list successfully created!');
         event.preventDefault();
     }
 
+    renderField(field){
+        const {meta: {touched, error}} = field;
+        const className = `form-group ${ touched && error ? 'has-danger' : ''}`;
+        return(
+            <div className={className}>
+                <label>Shopping list name</label>
+                <input
+                className = "form-control"
+                type = "text"
+                {...field.input}
+                />
+                <div className="text-help">
+                    { touched ? error : '' }
+                </div>
+            </div>
+        );
+    }
+
     render(){
+        const { handleSubmit } = this.props;
         return(
             <div>
-                <form onSubmit={this.handleSubmit}>
-                    <label>Listname:</label>
-                    <input type='text' name='listname' onChange={this.handleChange} />
-
-                    <input type='submit' value='submit' />
+                <form className="ShoppinglistForm col-sm-8" onSubmit={handleSubmit(this.handleSubmit.bind(this))}>
+                    <h2>Create a new shopping list</h2><br />
+                    <Field
+                        name = "listname"
+                        component = {this.renderField}
+                    />
+                    <button type="submit" className="btn btn-primary">Create List</button>
+                    <Link className="btn btn-primary" to="/">
+                    Cancel
+                    </Link> 
                 </form>
             </div>
         )
     }
 }
 
-export default ShoppinglistForm
+function validate (values){
+    const errors = {};
+    if(!values.listname){
+        errors.listname = "Please provide a listname!";
+    }
+    return errors;
+
+}
+
+export default reduxForm({
+    validate,
+    form: 'ShoppinglistForm'
+})(ShoppinglistForm);
