9aba491279cff8d201145916cb22b0978c48c21a
Betty-Kebenei <kebeneibetty13@gmail.com>
2018-01-12 11:35:04 +0300

    [FEATURE #154016569] Add action to capture login errors.

--- src/actions/Login.js
+++ src/actions/Login.js
@@ -1,14 +1,29 @@
 import axios from 'axios';
 
-// export const LOGIN_USER = 'login_user';
+export const LOGIN_USER = 'login_user';
+export const LOGIN_SUCCESS = 'login_success';
+export const LOGIN_ERROR = 'login_error';
 
-const ROOT_URL = 'http://localhost:5000'
+const ROOT_URL = 'http://localhost:5000';
 
 export function loginUser(values, callback){
-    const request = axios.post(`${ROOT_URL}/auth/login`, values)
-    .then(() => callback());
-    return{
-        type: 'LOGIN',
-        payload: request
+    return async (dispatch) => {
+        try {
+            const request = await axios.post(`${ROOT_URL}/auth/login`, values);
+            dispatch(
+                {
+                    type: LOGIN_SUCCESS
+                }
+            );
+            localStorage.setItem('user', request.data.token)
+            callback()
+        }catch(error){
+            dispatch(
+                {
+                    type: LOGIN_ERROR,
+                    payload: 'You email or password is invalid!'
+                }
+            );
+        }
     };
 }
