eb7ffc6bcc41839bfa3b8ee7474937c0e9038690
Betty-Kebenei <kebeneibetty13@gmail.com>
2018-01-12 11:55:12 +0300

    [FEATURE #154016569] Add a login reducer.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,12 @@
 import { combineReducers } from 'redux';
 import { reducer as formReducer } from 'redux-form';
 
-const rootReducer = combineReducers ({
-    form: formReducer
+import LoginReducer from './reducer_login';
 
+const rootReducer = combineReducers ({
+    form: formReducer,
+    user: LoginReducer
+    
 });
 
 export default rootReducer;
