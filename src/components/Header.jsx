71fdc60382653de9fdac4a4d693ba8dc66267f6c
Betty-Kebenei <kebeneibetty13@gmail.com>
2018-01-12 11:44:20 +0300

    [FEATURE #154237892] Add a function that adjusts the header based on if a user has been authenticated.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
-
-class Header extends React.Component {
+class Header extends Component {
+    renderChange(){
+        if(this.props.authenticated){
+            return (<button className="btn btn-primary">Logout</button>);
+        }
+        return (
+                <div></div>
+        );
+    }
     render(){
         return(
             <div className="Header col-sm-12">
-                <div className="Header col-sm-6">
+                <div className="col-sm-6">
                 <h1>Shopping List Tracker</h1>  
                 <p>We keep tracker on what you desire to spend your money on.</p>
                 </div>
-                <button className="btn btn-primary">Logout</button>
+                <div className="col-sm-6">{this.renderChange()}</div>
             </div>
         )
     }
 }
-
-export default Header;
+function mapStateToProps(state){
+    return {
+        authenticated: state.user.authenticated
+    };
+}
+export default connect(mapStateToProps)(Header);
