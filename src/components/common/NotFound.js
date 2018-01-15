48c368e40b64959a636e31a5d580241b51aecc7e
Betty-Kebenei <kebeneibetty13@gmail.com>
2018-01-15 20:19:49 +0300

    [FEATURE #154334747] Add a NotFound page.

---
+++ src/components/common/NotFound.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class NotFound extends Component {
+    
+    render(){
+        return(
+            <div>
+                <h1>404!</h1>
+                <p>A page with that URL cannot be found.</p>
+            </div>
+        );
+    };
+}
+export default NotFound;
