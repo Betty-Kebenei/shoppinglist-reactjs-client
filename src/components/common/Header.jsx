627dd10fa2e501b07a06f65555fa2c938117f4b9
Betty-Kebenei <kebeneibetty13@gmail.com>
2018-01-15 19:31:58 +0300

    [FEATURE #154295497] Add a logout link.

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,19 +1,22 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+
 import { logoutUser } from '../../actions/Login'
 class Header extends Component {
 
     onLogoutClick(){
-        console.log("Logout", this.props)
-        this.props.logoutUser((props) => {
-            this.props.history.push('/auth/login')  
-            alert('You are being logged out!'); 
-        }
-        );
+        this.props.logoutUser();
     }
+
     renderChange(){
         if(this.props.authenticated){
-            return (<button onClick={this.onLogoutClick.bind(this)} className="btn btn-primary">Logout</button>);
+            return (
+                
+                <Link onClick={this.onLogoutClick.bind(this)} className="btn btn-primary" to="/auth/logout">
+                Logout
+                </Link>
+            );
         }
         return (
                 <div></div>
@@ -26,7 +29,7 @@ class Header extends Component {
                 <h1>Shopping List Tracker</h1>  
                 <p>We keep tracker on what you desire to spend your money on.</p>
                 </div>
-                <div className="col-sm-6">{this.renderChange()}</div>
+                <div className="text-xs-rigth">{this.renderChange()}</div>
             </div>
         )
     }
