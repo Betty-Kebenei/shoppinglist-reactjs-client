354b5a29dd2b2b9ba7d9f20a8c25b7903c5506fb
Betty-Kebenei <kebeneibetty13@gmail.com>
2018-01-15 20:07:55 +0300

    Add calls to shoppinglists and shoppingitems reducers.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,10 +2,16 @@ import { combineReducers } from 'redux';
 import { reducer as formReducer } from 'redux-form';
 
 import LoginReducer from './reducer_login';
+import allshoppinglists from './reducer_shoppinglist';
+import oneshoppinglist from './reducer_activeShoppinglist';
+import shoppingitems from './reducer_shoppingitems';
 
 const rootReducer = combineReducers ({
     form: formReducer,
-    user: LoginReducer
+    user: LoginReducer,
+    allshoppinglists,
+    oneshoppinglist,
+    shoppingitems
     
 });
 
