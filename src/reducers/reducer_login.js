6b5420705f4e0aced7a7dbaebf8d92ac1e1c57a7
Betty-Kebenei <kebeneibetty13@gmail.com>
2018-01-15 20:08:59 +0300

    [FIX] Update the login reducer.

--- src/reducers/reducer_login.js
+++ src/reducers/reducer_login.js
@@ -1,14 +1,15 @@
 
-import { LOGIN_USER, LOGIN_SUCCESS, LOGIN_ERROR  } from '../actions/Login';
+import { LOGIN_SUCCESS, LOGIN_ERROR, NOT_LOGGEDIN } from '../actions/Login';
 
 export default function(state={}, action) {
   switch(action.type) {
     case LOGIN_SUCCESS:
       return { ...state, authenticated: true };
-    case LOGIN_USER:
+    case NOT_LOGGEDIN:
       return { ...state, authenticated: false };
     case LOGIN_ERROR:
       return { ...state, error: action.payload };
-  }
-  return state;
+    default:
+      return state;
+  } 
 }
