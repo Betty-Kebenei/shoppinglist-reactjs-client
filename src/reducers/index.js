9e6c37c12478d4289f17ed0f43683f736a59c120
Betty-Kebenei <kebeneibetty13@gmail.com>
2018-01-18 10:36:14 +0300

    [FEATURE #154295497] Set state to undefined on logout.
    Setting the state to undefined ensured that all states are
    reset upon logout; and therefore can no longer be accessed.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,12 +1,13 @@
 import { combineReducers } from 'redux';
 import { reducer as formReducer } from 'redux-form';
+import { NOT_LOGGEDIN } from '../actions/Login'
 
 import LoginReducer from './reducer_login';
 import allshoppinglists from './reducer_shoppinglist';
 import oneshoppinglist from './reducer_activeShoppinglist';
 import shoppingitems from './reducer_shoppingitems';
 
-const rootReducer = combineReducers ({
+const appReducer = combineReducers ({
     form: formReducer,
     user: LoginReducer,
     allshoppinglists,
@@ -14,6 +15,13 @@ const rootReducer = combineReducers ({
     shoppingitems
     
 });
+
+const rootReducer = (state, action) => {
+    if(action.type === NOT_LOGGEDIN ){
+        state == undefined
+    }
+    return appReducer(state, action);
+}
 
 export default rootReducer;
 
