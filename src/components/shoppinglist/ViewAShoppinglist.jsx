24b9dbea8c816984d96b205db2241bb819674864
Betty-Kebenei <kebeneibetty13@gmail.com>
2018-01-18 11:18:34 +0300

    [FEATURE #154334598] Display all shopping items in the viewshoppinglist component.
    This entails creating a table to display the shopping items of a specific
    Shopping list.
    
    Also, delete a single shopping list.

--- src/components/shoppinglist/ViewAShoppinglist.jsx
+++ src/components/shoppinglist/ViewAShoppinglist.jsx
@@ -1,50 +1,128 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import _ from 'lodash';
-import { getAllShoppingitems } from '../../actions/shoppingitems';
+import { Link } from 'react-router-dom';
 
+import { deleteShoppinglist } from '../../actions/Shoppinglist';
+import { getAllShoppingitems } from '../../actions/Shoppingitems';
 
 class ViewAShoppinglist extends Component {
+    constructor(props){
+        super(props)
+        this.renderShoppingitems = this.renderShoppingitems.bind(this)
+    }
 
     renderShoppingitems(){
+
         return (
             _.map(this.props.shoppingitems, item => {
                 return (
-                    <li 
-                        key={item.item_id}
-                        className="list-group-item">
-                        {item.itemname}
-                    </li>
+                    <tr key={item.item_id}>
+                        <td>{item.item_id}</td>
+                        <td>{item.itemname}</td>
+                        <td>{item.quantity}</td>
+                        <td>{item.price}</td>
+                        <td>
+                        <button 
+                            type="button" 
+                            className="btn glyphicon glyphicon-trash text-primary" 
+                            data-toggle="tooltip" 
+                            data-placement="top" 
+                            title="Delete_item" 
+                            />
+                        <button 
+                            type="button" 
+                            className="btn glyphicon glyphicon-edit text-primary" 
+                            data-toggle="tooltip" 
+                            data-placement="top" 
+                            title="Edit_item" />  
+                        </td>
+                    </tr>
                 );
             })
         );
     }
     
     render(){
+        
         if(!this.props.oneshoppinglist){
             return <div className="ViewShoppinglist col-sm-8">Select a shoppinglist to view</div>
         }
 
-        // this.props.getAllShoppingitems(this.props.oneshoppinglist.data.list_id);
-
+        const list_id = this.props.oneshoppinglist.data.list_id;
         return(
             <div className="ViewShoppinglist col-sm-8">
-            
-                <p><b>Shoppinglist_Id:</b><br />{this.props.oneshoppinglist.data.list_id} </p>
-                <p><b>Shoppinglistname:</b><br />{this.props.oneshoppinglist.data.listname} </p>
-                <p><b>ShoppingItems:</b><br /></p>
-                <ul>
-                    {this.renderShoppingitems}
-                </ul>
+                <div className="row">
+                    <div className="col-sm-12">
+                        <button 
+                            onClick={()=>{this.props.deleteShoppinglist(list_id)}}
+                            type="button" 
+                            className="btn glyphicon glyphicon-trash text-primary" 
+                            data-toggle="tooltip" 
+                            data-placement="top" 
+                            title="Delete_list" />
+                         <button 
+                            onClick={() => {}}
+                            type="button" 
+                            className="btn glyphicon glyphicon-edit text-primary" 
+                            data-toggle="tooltip" 
+                            data-placement="top" 
+                            title="Update_list" />
+                    </div>
+                    <br />
+                    <div className="col-sm-12">
+                        <p><b>Shoppinglist_Id:</b>{this.props.oneshoppinglist.data.list_id} </p>
+                        <p><b>Shoppinglistname:</b>{this.props.oneshoppinglist.data.listname} </p>
+                        <p><b>ShoppingItems:</b><br /></p>
+                        <button 
+                            type="button" 
+                            className="btn glyphicon glyphicon-trash text-primary" 
+                            data-toggle="tooltip" 
+                            data-placement="top" 
+                            title="Delete_all_items" 
+                            />
+                        <button 
+                            onClick={() => {this.props.getAllShoppingitems(list_id);}}
+                            type="button" 
+                            className="btn glyphicon glyphicon-file text-primary" 
+                            data-toggle="tooltip" 
+                            data-placement="top" 
+                            title="View_items" />
+                        <Link className="btn glyphicon glyphicon-plus text-primary"  
+                            data-toggle="tooltip" 
+                            data-placement="top" 
+                            title="Add_item"
+                            to="/additem" />
+                    </div>
+                    <br />
+                    <div className="col-sm-12">
+                        <table className="table bordered">
+                            <thead>
+                                <tr>
+                                    <td>ID</td>
+                                    <td>ITEMNAME</td>
+                                    <td>QUANTITY</td>
+                                    <td>PRICE</td>
+                                    <td>ACTIONS</td>
+                                    
+                                </tr>
+                            </thead>
+                            <tbody>
+                                    {this.renderShoppingitems()}
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
             </div>
         );
     };
 }
 function mapStateToProps(state){
+    
     return{
         oneshoppinglist: state.oneshoppinglist,
         shoppingitems: state.shoppingitems
     };
 }
 
-export default connect(mapStateToProps, { getAllShoppingitems } )(ViewAShoppinglist);
+export default connect(mapStateToProps, { deleteShoppinglist, getAllShoppingitems } )(ViewAShoppinglist);
