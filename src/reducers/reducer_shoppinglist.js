4a2fd5974f8059f9d145386e2a844e2f00718143
Betty-Kebenei <kebeneibetty13@gmail.com>
2018-01-18 10:43:23 +0300

    [FEATURE #154432226] Add delete shoppinglists reducer.

--- src/reducers/reducer_shoppinglist.js
+++ src/reducers/reducer_shoppinglist.js
@@ -1,10 +1,14 @@
 import _ from 'lodash';
-import { GET_ALLSHOPPINGLISTS_SUCCESS } from '../actions/Shoppinglist';
+import { GET_ALLSHOPPINGLISTS_SUCCESS, DELETE_ALLSHOPPINGLISTS_SUCCESS, DELETE_ONESHOPPINGLIST_SUCCESS } from '../actions/Shoppinglist';
 
 export default function(state = {}, action) {
     switch(action.type) {
         case GET_ALLSHOPPINGLISTS_SUCCESS:
             return _.mapKeys(action.payload.data.shoppinglists, 'list_id');
+        case DELETE_ALLSHOPPINGLISTS_SUCCESS:
+            return action.payload.data
+        case DELETE_ONESHOPPINGLIST_SUCCESS:
+            return action.payload.data
 
         default:
             return state;
