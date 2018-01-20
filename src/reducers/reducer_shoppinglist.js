580f941305e7e5f51c49ad81b27a99ff27c51ce5
Betty-Kebenei <kebeneibetty13@gmail.com>
2018-01-20 12:07:54 +0300

    [FEATURES #154495392 #154495394] Add update and paginate actions
    Add shopping list actions: Update and Paginate shopping lists.
    
    Also:
    Update initial states.
    Refactor by breaking a very long line of code; import actions line.

--- src/reducers/reducer_shoppinglist.js
+++ src/reducers/reducer_shoppinglist.js
@@ -1,10 +1,43 @@
 import _ from 'lodash';
-import { GET_ALLSHOPPINGLISTS_SUCCESS, DELETE_ALLSHOPPINGLISTS_SUCCESS, DELETE_ONESHOPPINGLIST_SUCCESS } from '../actions/Shoppinglist';
+import { 
+    GET_ALLSHOPPINGLISTS_SUCCESS, 
+    DELETE_ALLSHOPPINGLISTS_SUCCESS, 
+    DELETE_ONESHOPPINGLIST_SUCCESS,
+    UPDATE_SHOPPINGLIST_SUCCESS,
+    UPDATE_SHOPPINGLIST_ERROR,
+    PAGINATE_SHOPPINGLIST
+ } from '../actions/Shoppinglist';
 
-export default function(state = {}, action) {
+export default function(state = {
+    shoppinglists : [],
+    next : null,
+    previous : null
+}, action) {
     switch(action.type) {
         case GET_ALLSHOPPINGLISTS_SUCCESS:
-            return _.mapKeys(action.payload.data.shoppinglists, 'list_id');
+            return{
+                ...state,
+                shoppinglists: _.mapKeys(action.payload.data.shoppinglists, 'list_id')
+            }
+        case UPDATE_SHOPPINGLIST_SUCCESS:
+            return {
+                ...state,
+                updateSuccess: action.payload
+            }
+        case UPDATE_SHOPPINGLIST_ERROR:
+            return {
+                ...state,
+                updateError: action.payload
+
+            }
+        case PAGINATE_SHOPPINGLIST:
+            return{
+                ...state,
+                count: action.payload.data.count,
+                previous: action.payload.data.prev,
+                next: action.payload.data.next,
+                shoppinglists: action.payload.data.shoppinglists
+            }
         case DELETE_ALLSHOPPINGLISTS_SUCCESS:
             return action.payload.data
         case DELETE_ONESHOPPINGLIST_SUCCESS:
