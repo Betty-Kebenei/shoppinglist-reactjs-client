0dd7fb4f72765a5f9ffe7fbe26ef53ddaecdae9a
Betty-Kebenei <kebeneibetty13@gmail.com>
2018-02-10 08:51:09 +0300

    [FIX] Populate edit form and fix submit function.

--- src/components/shoppinglist/UpdateListForm.jsx
+++ src/components/shoppinglist/UpdateListForm.jsx
@@ -8,14 +8,14 @@ import RenderField from '../common/RenderField';
 import validate from '../common/Validation';
 
 export const UpdateListForm = (props) => {
-    const { handleSubmit, onSubmit } = props;
-
+    const { handleSubmit, onSubmit, listName, initialValues } = props;
     return(
         <div>
-            <form className="UpdateListForm col-sm-12" onSubmit={() => handleSubmit(onSubmit)}>
+            <form className="UpdateListForm col-sm-12" onSubmit={handleSubmit(onSubmit)}>
                 <Field
                     label = "Update List Name"
                     name = "listname"
+                    value = "gghh"
                     component = {RenderField}
                 />
                 <button type="submit" className="btn btn-primary">Update List</button>
