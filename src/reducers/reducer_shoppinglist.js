f2fc016beda806371692c7b132cfc75b94b06499
Betty-Kebenei <kebeneibetty13@gmail.com>
2018-02-10 08:47:07 +0300

    [FIX] Add search error reducer.

--- src/reducers/reducer_shoppinglist.js
+++ src/reducers/reducer_shoppinglist.js
@@ -4,7 +4,8 @@ import {
     DELETE_ALLSHOPPINGLISTS_SUCCESS, 
     DELETE_ONESHOPPINGLIST_SUCCESS,
     PAGINATE_SHOPPINGLIST,
-    SEARCH_SHOPPINGLIST
+    SEARCH_SHOPPINGLIST,
+    SEARCH_SHOPPINGLIST_ERROR
  } from '../actions/ShoppingLists';
 
 export default function(state = {
@@ -29,8 +30,14 @@ export default function(state = {
             return{
                 ...state,
                 count: action.payload.data.count,
-                shoppinglists: _.mapKeys(action.payload.data.shoppinglists, 'list_id')
+                shoppinglists:  _.mapKeys(action.payload.data.shoppinglists, 'list_id'),
+                errorMessage: ''
             }
+        case SEARCH_SHOPPINGLIST_ERROR:
+            return {
+                state: {},
+                errorMessage: action.payload.response.data.message
+            } 
         case DELETE_ALLSHOPPINGLISTS_SUCCESS:
             return action.payload.data
         case DELETE_ONESHOPPINGLIST_SUCCESS:
