f9c16448909f92733dbbfe51321c52ce470c997b
Betty-Kebenei <kebeneibetty13@gmail.com>
2018-02-10 09:10:06 +0300

    [TESTS #154016542] Test mapping function.

--- src/__tests__/shoppinglists/ViewLists.test.js
+++ src/__tests__/shoppinglists/ViewLists.test.js
@@ -2,9 +2,19 @@ import React from 'react';
 import {mount, shallow} from 'enzyme';
 import expect from 'expect';
 
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import configureStore from 'redux-mock-store';
+const middlewares = [];
+const mockStore = configureStore(middlewares);
+
 import ViewLists from '../../components/shoppinglist/ViewLists';
 
 describe('ViewLists', () => {
+    let store;
+    beforeEach(() => {
+        store = mockStore({});
+    });
     it('renders ViewLists component', () => {
         shallow(<ViewLists />)
     });
@@ -23,7 +33,29 @@ describe('ViewLists', () => {
     it("always renders a table", () => {
         const tables = shallow(<ViewLists />).find("table");
         expect(tables.length).toBeGreaterThan(0);
+        expect(tables.find('thead').length).toBeGreaterThan(0);
+        expect(tables.find('tbody').length).toBeGreaterThan(0);
     });
-
-    
+    it('renders mapping function', () => {
+        const shoppinglists = [
+            {
+                list_id: 1,
+                listname: "list1"
+            },
+            {
+                list_id: 2,
+                listname: "list2"
+            }
+        ]
+        const wrapper = mount(
+            <Provider store={store}>
+                <BrowserRouter>
+                    <ViewLists shoppinglists={shoppinglists}/>
+                </BrowserRouter>
+            </Provider>
+          );
+        expect(wrapper.find('tbody').children().length).toBeGreaterThan(0);
+        expect(wrapper.find('tbody').children().find('tr').length).toBeGreaterThan(0);
+        expect(wrapper.find('button').length).toBeGreaterThan(0);
+    });
 });
