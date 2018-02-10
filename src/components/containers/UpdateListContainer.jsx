18342c5754500895dd96e3e8389d9f92b12ce875
Betty-Kebenei <kebeneibetty13@gmail.com>
2018-02-10 08:55:33 +0300

    [FIX] Fetch data to populate edit list form from store.

--- src/components/containers/UpdateListContainer.jsx
+++ src/components/containers/UpdateListContainer.jsx
@@ -4,10 +4,15 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import toastr from 'toastr';
 
-import { updateShoppinglist } from '../../actions/ShoppingLists';
+import { updateShoppinglist, getOneShoppinglist } from '../../actions/ShoppingLists';
 import UpdateListForm from '../shoppinglist/UpdateListForm';
 
 export class UpdateListContainer extends Component{
+
+    componentDidMount(){
+        const { id } = this.props.match.params;
+        this.props.getOneShoppinglist(id);
+    }
 
     updateList = (values) => {
         const listId = this.props.match.params.id;
@@ -18,14 +23,25 @@ export class UpdateListContainer extends Component{
     }
 
     render(){
+        if(!this.props.shoppinglist){
+            return(<div>Loading....</div>);
+        }
+        const { listname } = this.props.shoppinglist.data
         return(
             <div>
                 <UpdateListForm 
                     onSubmit={this.updateList}
+                    listnName={this.props.match.params.listname}
+                    initialValues={{ listname }}
                 />
             </div>
         )
     }
 }
+const mapStateToProps = (state) => {
+    return{
+        shoppinglist: state.oneshoppinglist.singleShoppingList
+    }
+}
 
-export default connect(null, { updateShoppinglist })(UpdateListContainer);
+export default connect(mapStateToProps, { getOneShoppinglist, updateShoppinglist })(UpdateListContainer);
