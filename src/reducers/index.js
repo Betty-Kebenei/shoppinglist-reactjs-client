2dd7dfda8e5c2e78d8cbb587b65cfffdde72e149
Betty-Kebenei <kebeneibetty13@gmail.com>
2018-02-11 21:13:22 +0300

    [FIX] Refactor index reducer.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,37 +1,16 @@
 import { combineReducers } from 'redux';
-import { reducer as formReducer } from 'redux-form';
-import { NOT_LOGGEDIN } from '../actions/Login'
-import { POST_SHOPPINGLIST_SUCCESS } from '../actions/ShoppingLists';
 
-import LoginReducer from './reducer_login';
+import user from './reducer_login';
 import allshoppinglists from './reducer_shoppinglist';
 import oneshoppinglist from './reducer_activeShoppinglist';
 import shoppingitems from './reducer_shoppingitems';
+import form from './FormsReducer';
 
-const appReducer = combineReducers ({
-    form: formReducer.plugin({
-        ShoppinglistForm: (state, action) => {
-            switch(action.type){
-                case POST_SHOPPINGLIST_SUCCESS:
-                    return undefined;
-                default:
-                    return state;
-            }
-        }
-    }),
-    user: LoginReducer,
+const rootReducer = combineReducers ({
+    form,
+    user,
     allshoppinglists,
     oneshoppinglist,
-    shoppingitems
-    
+    shoppingitems 
 });
-
-const rootReducer = (state, action) => {
-    if(action.type === NOT_LOGGEDIN ){
-        state = undefined
-    }
-    return appReducer(state, action);
-}
-
 export default rootReducer;
-
