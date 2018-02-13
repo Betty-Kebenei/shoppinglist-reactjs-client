bf2cbcafebf459b53064c0335e09a8f3da5556f4
Betty-Kebenei <kebeneibetty13@gmail.com>
2018-02-13 12:45:38 +0300

    [FIX] Set state to undefined on logout.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -6,11 +6,22 @@ import oneshoppinglist from './reducer_activeShoppinglist';
 import shoppingitems from './reducer_shoppingitems';
 import form from './FormsReducer';
 
-const rootReducer = combineReducers ({
+import { NOT_LOGGEDIN } from '../actions/Login';
+
+const appReducer = combineReducers ({
     form,
     user,
     allshoppinglists,
     oneshoppinglist,
-    shoppingitems 
+    shoppingitems
+    
 });
+
+const rootReducer = (state, action) => {
+    if(action.type === NOT_LOGGEDIN ){
+        state = undefined
+    }
+    return appReducer(state, action);
+}
+
 export default rootReducer;
